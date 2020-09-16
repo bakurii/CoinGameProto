d277db244762dfc308717573aa13fe5516a69f25
Riina Backman <gr188369@gradia.fi>
2020-09-16 18:00:05 +0300

    Update

---
+++ Assets/Scripts/CoinsOnTheBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsOnTheBoard : MonoBehaviour
+{
+    public Transform[] wayPoints1 = new Transform[6];
+    public int wayPointIndex = 0;
+    GameObject player;
+    void Start()
+    {
+        player.transform.position = wayPoints1[wayPointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+}

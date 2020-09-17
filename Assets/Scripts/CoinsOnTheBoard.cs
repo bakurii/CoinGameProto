888c9fab0f48bb854951526a29c23a107744cb16
Riina Backman <gr188369@gradia.fi>
2020-09-17 14:09:46 +0300

    updates

--- Assets/Scripts/CoinsOnTheBoard.cs
+++ Assets/Scripts/CoinsOnTheBoard.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinsOnTheBoard : MonoBehaviour
 {
     public Transform[] wayPoints1 = new Transform[6];
     public int wayPointIndex = 0;
-    GameObject player;
+
+    public Image kisu;
+    public Transform playerPrefab;
+    public Button testButton;
+
     void Start()
     {
-        player.transform.position = wayPoints1[wayPointIndex].transform.position;
+
+        Button tButton = testButton.GetComponent<Button>();
+        tButton.onClick.AddListener(InstantiatePrefab);
+        
     }
 
     void Update()
     {
-        
+        //Instantiate(playerPrefab);
     }
+
+    void InstantiatePrefab()
+    {
+        Debug.Log("Jjee button painettu!");
+        Instantiate(kisu);
+    }
+
+
 }

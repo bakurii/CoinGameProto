40781eb07f1d34687a1d4d88abe0c58173c5dda0
Riina Backman <gr188369@gradia.fi>
2020-10-01 13:45:21 +0300

    Updates

---
+++ Assets/Scripts/Player1Coins.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsOnTheBoard : MonoBehaviour
+{
+    //Every row has its own waypoint list.
+    [SerializeField]
+    Transform[] waypointsList0;
+    [SerializeField]
+    Transform[] waypointsList1;
+
+    //public float movementSpeed = 2f;
+    private int wayPointIndex = 5;
+    private int loopNumber = 5;
+
+    private int testNum = 0;
+
+    public Image kisu;
+    //public Transform playerPrefab;
+    public Button button0;
+    public Button button1;
+
+    void Start()
+    {
+        //transform.position = waypointsList[wayPointIndex].transform.position;
+        Button zeroButton = button0.GetComponent<Button>();
+        Button oneButton = button1.GetComponent<Button>();
+        zeroButton.interactable = true;
+        zeroButton.onClick.AddListener(InstantiatePrefabZero);
+        oneButton.onClick.AddListener(InstantiatePrefabOne);
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    int testArrayNumber()
+    {
+
+
+        return testNum;
+    }
+
+    void InstantiatePrefabZero()
+    {
+        Debug.Log("Jjee button 0 painettu!");
+        
+        //Path/object where images are instantiated 
+        Transform imagePath = GameObject.FindWithTag("Path").transform;
+
+        for (int i = 5; i >= loopNumber; i--)
+        {
+            wayPointIndex = i;
+            Instantiate(kisu, waypointsList0[wayPointIndex].transform.position, Quaternion.identity, imagePath);
+        }
+
+        loopNumber--;
+        
+        //Disables butoon after loopnumber is 0.
+        if (loopNumber < 0)
+        {
+            button0.interactable = false;
+        }
+    }
+
+    void InstantiatePrefabOne()
+    {
+        Debug.Log("Jjee button 1 painettu!");
+
+        //Path/object where images are instantiated 
+        Transform imagePath = GameObject.FindWithTag("Path").transform;
+
+        for (int i = 5; i >= loopNumber; i--)
+        {
+            wayPointIndex = i;
+            Instantiate(kisu, waypointsList1[wayPointIndex].transform.position, Quaternion.identity, imagePath);
+        }
+
+        loopNumber--;
+
+        //Disables button after loopnumber is 0.
+        if (loopNumber < 0)
+        {
+            button0.interactable = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Turns.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { START, P1, P2, WIN, LOSE}
+
+//https://www.youtube.com/watch?v=_1pz_ohupPs
+//Tutorial video for turns.
+
+public class Turns : MonoBehaviour
+{
+    public GameState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = GameState.START;
+        SetupStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetupStart()
+    {
+
+        state = GameState.P1;
+        Player1Turn();
+    }
+
+    void Player1Turn()
+    {
+
+    }
+}
